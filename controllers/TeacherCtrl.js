90f7c0bc21e8dec4912daddbedf45f019542d2d3
Firas bechedli <fbe.chedli@gmail.com>
2025-11-25 16:21:51 +0100

    bcrypt

--- controllers/TeacherCtrl.js
+++ controllers/TeacherCtrl.js
@@ -1,5 +1,5 @@
 const Teacher = require("../models/TeacherModel"); // Import the Teacher model
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Users = require("../models/userModel"); // 👈 add this
 
