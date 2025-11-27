2e8f99166904c34c4740461837d38be31a387ef8
Firas bechedli <fbe.chedli@gmail.com>
2025-11-27 13:30:25 +0100

    cors11

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -37,7 +37,7 @@ router.get('/logout', userCtrl.logout)
 
 router.get('/refresh_token', userCtrl.refreshToken)
 
-router.get('/infor',  userCtrl.getUser)
+router.get('/infor', auth,  userCtrl.getUser)
 
 router.get('/all_users', userCtrl.getAllUsers);
 
